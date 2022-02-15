0d9b8c064d6386baf80b459a96e3680fae8d846f
Simran <59317835+simrannakarmi@users.noreply.github.com>
2022-02-16 00:12:15 +0545

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,17 @@
+public class Pyramid {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = i; j <= 4; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            for (int l = 1; l <= i - 1; l++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
